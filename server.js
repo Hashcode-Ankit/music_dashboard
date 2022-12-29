b0ca24fed01cb7c05bd08af6863e149fa89ee1a5
Bhavuksharmaa <9671738539h@gmail.com>
2022-12-29 17:42:53 +0530

    xyz

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
-var exphbs =require('express-handlebars')
-var path=require("path");
+var exphbs = require('express-handlebars')
+var path = require("path");
 var app = express();
 http = require('http')
 
@@ -10,41 +10,51 @@ app.use(express.static(__dirname + '/assets'));
 
 // To set sidebar and navbar as component 
 app.engine('hbs', exphbs.engine({
-  extname : '.hbs',
+    extname: '.hbs',
 }));
 app.set('view engine', 'handlebars');
 
 // dashboard page 
-app.get("/", async function(req,res){
-res.render(path.join(__dirname,"/views/overview.hbs"))
+app.get("/", async function(req, res) {
+    var viewData = {
+        total_albums: 8,
+        account_balance: 90,
+        draft_count: 4
+    }
+    res.render(path.join(__dirname, "/views/overview2.hbs"), { data: viewData })
 });
 // new release page 
-app.get("/new-release", async function(req,res){
-res.render(path.join(__dirname,"/views/forms.hbs"))
+app.get("/new-release", async function(req, res) {
+    res.render(path.join(__dirname, "/views/new_release.hbs"))
 });
 // music catalog page
-app.get("/music-catalog", async function(req,res){
-res.render(path.join(__dirname,"/views/cards.hbs"))
+app.get("/music-catalog", async function(req, res) {
+    var viewData = {
+        total_albums: 8,
+        account_balance: 90,
+        draft_count: 4
+    }
+    res.render(path.join(__dirname, "/views/overview2.hbs"), { data: viewData })
 });
 // label management page
-app.get("/label-manage", async function(req,res){
-res.render(path.join(__dirname,"/views/charts.hbs"))
+app.get("/label-manage", async function(req, res) {
+    res.render(path.join(__dirname, "/views/charts.hbs"))
 });
 // artist management page
-app.get("/artist-manage", async function(req,res){
-res.render(path.join(__dirname,"/views/buttons.hbs"))
+app.get("/artist-manage", async function(req, res) {
+    res.render(path.join(__dirname, "/views/buttons.hbs"))
 });
 // finance management page
-app.get("/finance-manage", async function(req,res){
-res.render(path.join(__dirname,"/views/modals.hbs"))
+app.get("/finance-manage", async function(req, res) {
+    res.render(path.join(__dirname, "/views/modals.hbs"))
 });
 // analytics manage page 
-app.get("/analytics-manage", async function(req,res){
-res.render(path.join(__dirname,"/views/tables.hbs"))
+app.get("/analytics-manage", async function(req, res) {
+    res.render(path.join(__dirname, "/views/tables.hbs"))
 });
 // you tube request page
-app.get("/you-tube-req", async function(req,res){
-res.render(path.join(__dirname,"/views/overview.hbs"))
+app.get("/you-tube-req", async function(req, res) {
+    res.render(path.join(__dirname, "/views/overview.hbs"))
 });
 
 // listening on 8080 
